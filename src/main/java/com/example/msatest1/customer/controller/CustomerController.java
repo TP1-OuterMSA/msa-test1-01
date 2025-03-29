3d41a92557484b189eb574b53a217b66daacf7f4
choieuntaek <choiet0225@gmail.com>
2025-03-29 16:25:17 +0900

    test2

--- src/main/java/com/example/msatest1/customer/controller/CustomerController.java
+++ src/main/java/com/example/msatest1/customer/controller/CustomerController.java
@@ -12,7 +12,7 @@ public class CustomerController {
     private CustomerService customerService;
     @PostMapping("/register")
     public ResponseEntity<Customer> registerCustomer(@RequestBody Customer customer) {
-        customer.setName(customer.getName() + " cicd 테스트");
+        customer.setName(customer.getName() + " cicd 테스트2");
         Customer savedCustomer = customerService.registerCustomer(customer);
         return new ResponseEntity<>(savedCustomer, HttpStatus.CREATED);
     }
